9402ca1e7b96aea476867dcd8f27b584f89b034a
Greg Graham <greg@grahamtx.net>
2019-06-19 14:27:31 -0500

    Added start screen and play again capability.

--- Game1.cs
+++ Game1.cs
@@ -7,6 +7,13 @@ using System;
 
 namespace InvadersCS
 {
+    enum GameStates
+    {
+        StartScreen,
+        Play,
+        EndScreen
+    }
+
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
@@ -47,6 +54,8 @@ namespace InvadersCS
         private int _nextPlasmaPos = 0;
         private Vector2[] _saucerPositions = new Vector2[_numSaucers];
 
+        private GameStates _state = GameStates.StartScreen;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this)
@@ -67,6 +76,12 @@ namespace InvadersCS
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            InitializePositions();
+            base.Initialize();
+        }
+
+        private void InitializePositions()
+        {
             _shipPosition = new Vector2(_width / 2, _height - 80);
             for (int i = 0; i < _numPlasmas; i++)
             {
@@ -78,8 +93,6 @@ namespace InvadersCS
                 _saucerPositions[i] = new Vector2(_rand.Next(_width), saucer_y);
                 saucer_y -= 80;
             }
-
-            base.Initialize();
         }
 
         /// <summary>
@@ -125,79 +138,110 @@ namespace InvadersCS
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+
             // TODO: Add your update logic here
-            if (_invasions < _gameOverInvasions)
+            var kstate = Keyboard.GetState();
+
+            switch (_state)
             {
-                var kstate = Keyboard.GetState();
-
-                if (kstate.IsKeyDown(Keys.Left) && _shipPosition.X > -8)
-                {
-                    _shipPosition.X -= _shipSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                }
-
-                if (kstate.IsKeyDown(Keys.Right) && _shipPosition.X < _width + 8)
-                {
-                    _shipPosition.X += _shipSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                }
-
-                if (kstate.IsKeyDown(Keys.Space) && gameTime.TotalGameTime > _fireTimer)
-                {
-                    _plasmaPositions[_nextPlasmaPos].X = _shipPosition.X;
-                    _plasmaPositions[_nextPlasmaPos].Y = _shipPosition.Y - 6;
-                    _nextPlasmaPos++;
-                    if (_nextPlasmaPos >= _numPlasmas)
+                case GameStates.StartScreen:
+                    if (kstate.IsKeyDown(Keys.Space))
                     {
-                        _nextPlasmaPos = 0;
+                        _state = GameStates.Play;
                     }
-                    _fireTimer = gameTime.TotalGameTime + _fireDelay;
-                    _shoot.Play();
-                }
+                    break;
 
-                for (int j = 0; j < _numPlasmas; j++)
-                {
-                    if (IsOnScreen(_plasmaPositions[j]))
+                case GameStates.Play:
+                    if (_invasions < _gameOverInvasions)
                     {
-                        _plasmaPositions[j].Y -= _plasmaSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                        for (int i = 0; i < _saucerPositions.Length; i++)
+                        if (kstate.IsKeyDown(Keys.Left) && _shipPosition.X > -8)
                         {
-                            if (Collision(_saucerTexture, _saucerPositions[i], _plasmaTexture, _plasmaPositions[j]))
+                            _shipPosition.X -= _shipSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                        }
+
+                        if (kstate.IsKeyDown(Keys.Right) && _shipPosition.X < _width + 8)
+                        {
+                            _shipPosition.X += _shipSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                        }
+
+                        if (kstate.IsKeyDown(Keys.Space) && gameTime.TotalGameTime > _fireTimer)
+                        {
+                            _plasmaPositions[_nextPlasmaPos].X = _shipPosition.X;
+                            _plasmaPositions[_nextPlasmaPos].Y = _shipPosition.Y - 6;
+                            _nextPlasmaPos++;
+                            if (_nextPlasmaPos >= _numPlasmas)
                             {
-                                _score++;
-                                _saucerPositions[i] = ResetSaucer(_saucerPositions[i]);
-                                _plasmaPositions[j] = new Vector2(-10, -10);
-                                _explosion.Play();
+                                _nextPlasmaPos = 0;
+                            }
+                            _fireTimer = gameTime.TotalGameTime + _fireDelay;
+                            _shoot.Play();
+                        }
+
+                        for (int j = 0; j < _numPlasmas; j++)
+                        {
+                            if (IsOnScreen(_plasmaPositions[j]))
+                            {
+                                _plasmaPositions[j].Y -= _plasmaSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                                for (int i = 0; i < _saucerPositions.Length; i++)
+                                {
+                                    if (Collision(_saucerTexture, _saucerPositions[i], _plasmaTexture, _plasmaPositions[j]))
+                                    {
+                                        _score++;
+                                        _saucerPositions[i] = ResetSaucer(_saucerPositions[i]);
+                                        _plasmaPositions[j] = new Vector2(-10, -10);
+                                        _explosion.Play();
+                                    }
+                                }
                             }
                         }
-                    }
-                }
 
-                for (var i = 0; i < _saucerPositions.Length; i++)
-                {
-                    _saucerPositions[i].Y += _saucerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                    if ((_saucerPositions[i].Y - _saucerTexture.Height / 2) > _height)
-                    {
-                        _invasions++;
-                        _saucerPositions[i] = ResetSaucer(_saucerPositions[i]);
-                        continue;
-                    }
-
-                    if (_rand.Next(2) == 1)
-                    {
-                        _saucerPositions[i].X += _saucerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                        if (_saucerPositions[i].X > _width)
+                        for (var i = 0; i < _saucerPositions.Length; i++)
                         {
-                            _saucerPositions[i].X = _width;
+                            _saucerPositions[i].Y += _saucerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                            if ((_saucerPositions[i].Y - _saucerTexture.Height / 2) > _height)
+                            {
+                                _invasions++;
+                                _saucerPositions[i] = ResetSaucer(_saucerPositions[i]);
+                                continue;
+                            }
+
+                            if (_rand.Next(2) == 1)
+                            {
+                                _saucerPositions[i].X += _saucerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                                if (_saucerPositions[i].X > _width)
+                                {
+                                    _saucerPositions[i].X = _width;
+                                }
+                            }
+                            else
+                            {
+                                _saucerPositions[i].X -= _saucerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                                if (_saucerPositions[i].X < 0)
+                                {
+                                    _saucerPositions[i].X = 0;
+                                }
+                            }
                         }
                     }
                     else
                     {
-                        _saucerPositions[i].X -= _saucerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                        if (_saucerPositions[i].X < 0)
-                        {
-                            _saucerPositions[i].X = 0;
-                        }
+                        _state = GameStates.EndScreen;
                     }
-                }
+                    break;
+
+                case GameStates.EndScreen:
+                    if (kstate.IsKeyDown(Keys.Y))
+                    {
+                        _score = 0;
+                        _invasions = 0;
+                        InitializePositions();
+                        _state = GameStates.StartScreen;
+                    }
+                    else if (kstate.IsKeyDown(Keys.N))
+                    {
+                        Exit();
+                    }
+                    break;
             }
             base.Update(gameTime);
         }
@@ -267,25 +311,36 @@ namespace InvadersCS
             GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
+
             _spriteBatch.Begin();
             _spriteBatch.Draw(_starsTexture, new Vector2(0, 0), Color.White);
-            _spriteBatch.DrawString(_font, $"score={_score}, invasions={_invasions}",
-                                   new Vector2(10, _height - 30), Color.Lime);
-            if (_invasions >= _gameOverInvasions)
+            switch (_state)
             {
-                _spriteBatch.DrawString(_font, "Game Over", new Vector2(277, _height / 2), Color.Lime);
-            }
-            else
-            {
-                DrawCentered(_shipTexture, _shipPosition);
-                foreach (var pos in _plasmaPositions)
-                {
-                    DrawCentered(_plasmaTexture, pos);
-                }
-                foreach (var pos in _saucerPositions)
-                {
-                    DrawCentered(_saucerTexture, pos);
-                }
+                case GameStates.StartScreen:
+                    _spriteBatch.DrawString(_font, "Invaders", new Vector2(277, _height * 0.45f), Color.Lime);
+                    _spriteBatch.DrawString(_font, "Press Space to start.", new Vector2(230, _height * 0.55f), Color.Lime);
+                    break;
+
+                case GameStates.Play:
+                    _spriteBatch.DrawString(_font, $"score={_score}, invasions={_invasions}",
+                                           new Vector2(10, _height - 30), Color.Lime);
+                    DrawCentered(_shipTexture, _shipPosition);
+                    foreach (var pos in _plasmaPositions)
+                    {
+                        DrawCentered(_plasmaTexture, pos);
+                    }
+                    foreach (var pos in _saucerPositions)
+                    {
+                        DrawCentered(_saucerTexture, pos);
+                    }
+                    break;
+
+                case GameStates.EndScreen:
+                    _spriteBatch.DrawString(_font, $"score={_score}, invasions={_invasions}",
+                                           new Vector2(10, _height - 30), Color.Lime);
+                    _spriteBatch.DrawString(_font, "Game Over", new Vector2(277, _height * 0.45f), Color.Lime);
+                    _spriteBatch.DrawString(_font, "Do you want to play again (Y/N)?", new Vector2(200, _height * 0.55f), Color.Lime);
+                    break;
             }
 
             _spriteBatch.End();
